bc0cdd9eadda6083602aff38a2b6370f1f5c5dc3
emmaauger <83782933+emmaauger@users.noreply.github.com>
2024-11-20 11:21:03 -0500

    small edits

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ function App() {
   return (
     <div className="App">
       Weather App
-      <footer>
-        This project is created by Emma Auger Hirner and is
-        <a href="https://github.com/emmaauger/weather-react" target="_blank">
-          open-sourced on github
-        </a>
-      </footer>
+      <footer>This project is created by Emma Auger Hirner and is</footer>
     </div>
   );
 }
