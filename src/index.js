bbf618c58a29b6ef884f887a0a319c0254d213cc
emmaauger <83782933+emmaauger@users.noreply.github.com>
2025-02-07 13:39:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import App from "./App.js";
+import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
